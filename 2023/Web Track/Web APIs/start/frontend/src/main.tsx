bfd3a7dae99c32ac5467f9d7ff34b8540139700e
Aaron Powell <me@aaron-powell.com>
2023-10-23 02:32:23 +0000

    adding fluentui

--- 2023/Web Track/Web APIs/start/frontend/src/main.tsx
+++ 2023/Web Track/Web APIs/start/frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <FluentProvider theme={webLightTheme}>
+      <App />
+    </FluentProvider>
+  </React.StrictMode>
+);
+
